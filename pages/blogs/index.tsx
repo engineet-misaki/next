1c9a65ffa735d2bf9901534ac6323db3b949494c
misaki toyama <toyamax2020@gmail.com>
2021-12-12 14:31:26 +0900

    レスポンシブ

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link';
 
 const Home = ({ blogs }) => {
   return (
-    <div className='overflow-hidden h-screen bg-gray-100'>
+    <div className='overflow-hidden min-h-screen bg-gray-100'>
       <div className='bg-white'>
         <h2 className='container p-6 mx-auto text-2xl font-bold'>記事一覧</h2>
       </div>
       <div className='container mx-auto'>
-        <div className='py-8 px-16 m-16 bg-white rounded-lg shadow-sm'>
+        <div className='p-6 md:py-8 md:px-16 m-3 md:m-16 my-8 bg-white rounded-lg shadow-sm'>
           {blogs.map((blog) => (
             <article key={blog.id} className='my-6'>
               <Link href='/blogs/[id]' as={`blogs/${blog.id}`}>
@@ -18,9 +18,9 @@ const Home = ({ blogs }) => {
                     alt='thumbnail'
                     width={64}
                     height={64}
-                    className='pr-3 rounded-full'
+                    className='block w-16 rounded-full'
                   />
-                  <div className='ml-6'>
+                  <div className='ml-6 w-10/12'>
                     <h2 className='text-lg'>{blog.title}</h2>
                     <p className='text-xs text-gray-400'>
                       {blog.updatedAt} {blog.auther}
